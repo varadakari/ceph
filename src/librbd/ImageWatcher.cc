dfb69fa3cec479259e9596c3169cf342972e2b35
Jason Dillaman <dillaman@redhat.com>
2015-05-07 12:35:36 -0400

    librbd: don't hold owner_lock for write during flush
    The various IO callback codepaths will attempt to take
    the lock, which will result in deadlock since the flush
    cannot complete.
    
    Backport: hammer
    Fixes: #11537
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/ImageWatcher.cc
+++ src/librbd/ImageWatcher.cc
@@ -377,18 +377,19 @@ bool ImageWatcher::release_lock()
     return false;
   }
   prepare_unlock();
-
   m_image_ctx.owner_lock.put_write();
-  m_image_ctx.cancel_async_requests();
-  m_image_ctx.owner_lock.get_write();
 
-  if (!is_lock_owner()) {
-    return false;
-  }
+  m_image_ctx.cancel_async_requests();
 
   {
-    RWLock::WLocker l2(m_image_ctx.md_lock);
+    RWLock::RLocker owner_locker(m_image_ctx.owner_lock);
+    RWLock::WLocker md_locker(m_image_ctx.md_lock);
     librbd::_flush(&m_image_ctx);
+  }
+
+  m_image_ctx.owner_lock.get_write();
+  if (!is_lock_owner()) {
+    return false;
   }
 
   unlock();
